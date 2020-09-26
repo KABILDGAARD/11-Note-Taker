a9bf33f79b1aa50bbc24e860b74fa9c8b05336fd
mona <k.abildgaard.photography@gmail.com>
2020-09-26 12:16:58 -0700

    server

--- server.js
+++ server.js
@@ -11,7 +11,4 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-const server = app.listen(process.env.PORT || 8000, function () {
-    const port = server.address().port;
-    console.log("Express is working on port " + port);
-  });
+app.listen(PORT, () => console.log("Server is running on " + PORT));
